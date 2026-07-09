61275356bd28c3993bd54a2e42ffb0453db2479e
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-07-09 16:32:37 +0530

    Time: 4 ms (63.85%), Space: 19.3 MB (68.28%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(V);
+        for(auto& it: prerequisites){
+            int u = it[0];
+            int v = it[1];
+
+            adj[u].push_back(v);
+        }
+        vector<int> indegree(V,0);
+        for(int i=0;i<V;i++){
+            for(auto it: adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
+            }
+        }
+        if(ans.size()==V)return true;
+        return false;
+    }
+};
