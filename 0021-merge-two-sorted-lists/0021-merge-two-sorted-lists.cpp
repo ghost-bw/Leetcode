5838d3aeaccce066783d29c9fb74ed203bc0e5a5
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-07-11 12:44:01 +0530

    Time: 0 ms (100%), Space: 19.3 MB (87.65%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,48 +1,45 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* curr=new ListNode(0);
-        ListNode* dummy=curr;
+       
+        if(list1 == NULL)
+            return list2;
+		
+        if(list2 == NULL)
+            return list1;
         
-        while(list1!=nullptr && list2!=nullptr){
-            int val=0;
-            if(list1->val<=list2->val){
-                val=list1->val;
-                list1=list1->next;
-            }else{
-                val=list2->val;
-                list2=list2->next;
+        ListNode * ptr = list1;
+        if(list1 -> val > list2 -> val)
+        {
+            ptr = list2;
+            list2 = list2 -> next;
+        }
+        else
+        {
+            list1 = list1 -> next;
+        }
+        ListNode *curr = ptr;
+        
+        while(list1 &&  list2)
+        {
+            if(list1 -> val < list2 -> val){
+                curr->next = list1;
+                list1 = list1 -> next;
             }
-            dummy->next=new ListNode(val);
-            dummy=dummy->next;
+            else{
+                curr->next = list2;
+                list2 = list2 -> next;
+            }
+            curr = curr -> next;
+                
         }
-         while (list1 != nullptr) {
-
-            dummy->next = new ListNode(list1->val);
-
-            dummy = dummy->next;
-
-            list1 = list1->next;
-        }
-
-        while (list2 != nullptr) {
-
-            dummy->next = new ListNode(list2->val);
-
-            dummy = dummy->next;
-
-            list2 = list2->next;
-        }
-        return curr->next;
+		
+        if(!list1)
+            curr -> next = list2;
+        else
+            curr -> next = list1;
+            
+        return ptr;
+       
     }
 };
