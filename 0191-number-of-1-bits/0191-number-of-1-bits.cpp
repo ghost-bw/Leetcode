c8ef373dcf39f76af473592b3827933609c7e161
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-07-20 16:57:00 +0530

    Time: 0 ms (100%), Space: 8.1 MB (82.89%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int hammingWeight(int n) {
         if(n==0) return 0;
-        string res="";
+        
         int count=0;
         while(n){
             if(n&1)count++;
-            res+=(n&1)+'0';
+           
             n=n>>1;
         }
         
