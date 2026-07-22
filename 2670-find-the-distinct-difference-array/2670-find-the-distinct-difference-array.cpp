a67f207e8450072d34857a767a8e8ea522ac2d05
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-07-23 00:00:54 +0530

    Time: 18 ms (48.46%), Space: 98.4 MB (54.32%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cpp
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     vector<int> distinctDifferenceArray(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> pre(n), suf(n), ans;
-        unordered_set<int> s;
+        unordered_map<int,int> f;
+        for (int x : nums) f[x]++;
 
-        for (int i = 0; i < n; i++) {
-            s.insert(nums[i]);
-            pre[i] = s.size();
-        }
+        unordered_set<int> l;
+        vector<int> ans;
 
-        s.clear();
+        for (int x : nums) {
+            l.insert(x);
 
-        for (int i = n - 1; i >= 0; i--) {
-            s.insert(nums[i]);
-            suf[i] = s.size();
-        }
+            f[x]--;
+            if (f[x] == 0) f.erase(x);
 
-        for (int i = 0; i < n; i++) {
-            int left = pre[i];
-            int right = (i == n - 1) ? 0 : suf[i + 1];
-            ans.push_back(left - right);
+            ans.push_back(l.size() - f.size());
         }
 
         return ans;
