33e6d831af8c8863524dcb3219582d4c2faf05b1
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-07-25 18:46:10 +0530

    Time: 4 ms (30.82%), Space: 16.1 MB (50.06%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    bool isanagram(string a, string b) {
-        if (a.size() != b.size()) return false;
-
-        unordered_map<char, int> mp;
-
-        for (char c : a) mp[c]++;
-        for (char c : b) mp[c]--;
-
-        for (auto it : mp) {
-            if (it.second != 0) return false;
-        }
-        return true;
-    }
-
     vector<string> removeAnagrams(vector<string>& words) {
-        vector<string> ans;
 
-        for (string &w : words) {
-            if (ans.empty() || !isanagram(ans.back(), w))
-                ans.push_back(w);
+        vector<string> ans;
+        unordered_map<string, int> prev;
+
+        for (string word : words) {
+            string key = word;
+            sort(key.begin(), key.end());
+
+            if (prev.empty() || prev.find(key) == prev.end()) {
+                ans.push_back(word);
+                prev.clear();      // Forget older groups
+                prev[key] = 1;     // Remember only previous kept word
+            }
         }
 
         return ans;
