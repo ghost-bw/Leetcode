8cb4ef26829bf8852364d2821914fda37b197f17
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-07-25 17:51:48 +0530

    Time: 74 ms (7.4%), Space: 34.4 MB (10.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -4,8 +4,14 @@ public:
           unordered_map<string, vector<string>> mp;
 
         for (string s : strs) {
-            string key = s;
-            sort(key.begin(), key.end());
+            vector<int> freq(26,0);
+            for(char c: s){
+                freq[c-'a']++;
+            }
+            string key;
+            for (int x : freq){
+                key += to_string(x) + '#';
+            }
             mp[key].push_back(s);
         }
 
