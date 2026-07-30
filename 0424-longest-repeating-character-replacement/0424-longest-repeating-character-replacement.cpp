9259a5ad7d9a2c1dbd3cd8205c7c450df1ff4b9c
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-07-30 22:33:26 +0530

    Time: 0 ms (100%), Space: 10.8 MB (93.78%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> occurrence(26, 0);
+
+        int left = 0;
+        int maxOccurrence = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            maxOccurrence = max(maxOccurrence, ++occurrence[s[right] - 'A']);
+
+            if (right - left + 1 - maxOccurrence > k) {
+                occurrence[s[left] - 'A']--;
+                left++;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
