905288e8d4f86392339f2f3f920e0f8413097354
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-07-31 23:57:22 +0530

    Time: 4 ms (27.01%), Space: 18.1 MB (79.93%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -2,15 +2,10 @@ class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
         int n=arr.size();
-        int sum1=0,sum2=0;
         sort(arr.begin(),arr.end());
         sort(target.begin(),target.end());
         if(arr!=target)return false;
-        for(int i=0;i<n;i++){
-            sum1+=target[i];
-            sum2+=arr[i];
-        }
-        if(sum1!=sum2)return false;
+
         return true;
     }
 };
