61aa4f1ec7b485906a52d154f2e392af30459fa8
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-08-08 23:23:17 +0530

    Time: 0 ms (100%), Space: 11.9 MB (18.46%) - LeetHub

---
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numSub(string s) {
+        
+        long long ones=0;
+        long long ans=0;
+        for(char c: s){
+            if(c=='1'){
+                ones++;
+                ans+=ones;
+            }else{
+                ones=0;
+            }
+        }
+        return ans%1000000007;
+
+    }
+};
