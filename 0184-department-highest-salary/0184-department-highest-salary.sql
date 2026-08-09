161a85a97a4043e1e326bdb5387d9f6f6e8c69fb
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-08-09 17:28:34 +0530

    Time: 624 ms (94.39%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,7 +1,12 @@
 # Write your MySQL query statement below
-select d.name as Department,e.name as Employee,e.salary as Salary from Employee e
-join Department d on e.departmentId =d.id
-where salary=(
-    select max(e2.salary) from Employee e2
-    where e2.departmentId=e.departmentId
-);
+-- select d.name as Department,e.name as Employee,e.salary as Salary from Employee e
+-- join Department d on e.departmentId =d.id
+-- where salary=(
+--     select max(e2.salary) from Employee e2
+--     where e2.departmentId=e.departmentId
+-- );
+select Department,Employee,Salary from (
+    select d.name as Department,e.name as Employee,e.salary 
+    ,dense_rank() over(partition by d.name order by salary desc ) as rnk from Employee e
+    join Department d on e.departmentId =d.id ) as temp
+where rnk=1;
