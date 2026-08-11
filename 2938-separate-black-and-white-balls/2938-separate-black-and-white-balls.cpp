817fd8feec2e00958cb7c7b24b2f4b45a198c7be
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-08-11 19:19:57 +0530

    Time: 3 ms (85.04%), Space: 17.2 MB (44.05%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long ones=0;
+        long long ans=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='1')ones++;
+            else{
+                ans+=ones;
+            }
+        }
+        
+        return ans;
+    }
+};
