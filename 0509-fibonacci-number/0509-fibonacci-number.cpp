6165f7e39f5423d04b96c85ab0a6b7d28420bbe9
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-08-19 00:15:54 +0530

    Time: 3 ms (64.66%), Space: 8.1 MB (17.23%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    int solve(int n,vector<int> &dp){
+        if(n<=1){
+            dp[n]=n;
+            return dp[n];
+        }
+        if(dp[n]!=-1)return dp[n];
+        dp[n]= solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
+    }
     int fib(int n) {
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        return fib(n-1)+fib(n-2);
-    
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
