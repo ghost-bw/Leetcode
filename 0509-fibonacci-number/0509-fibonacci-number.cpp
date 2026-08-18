da436accd5654f76803658ef99b4008634c74359
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-08-19 01:28:50 +0530

    Time: 0 ms (100%), Space: 7.8 MB (65.71%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -12,12 +12,14 @@ public:
     int fib(int n) {
         if (n <= 1)
         return n;
-        vector<int> dp(n+1,0);
-        dp[0]=0;
-        dp[1]=1;
+        int prev2=0;
+        int prev1=1;
+        int ans=0;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            ans=prev2+prev1;
+            prev2=prev1;
+            prev1=ans;
         }
-        return dp[n];
+        return ans;
     }
 };
