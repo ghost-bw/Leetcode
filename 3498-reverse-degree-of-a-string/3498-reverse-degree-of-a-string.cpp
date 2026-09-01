8419ed5e205f627b23f372723de93982d6f23c64
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-09-01 23:59:51 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.66%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int reverseDegree(string s) {
         int ans=0;
-        vector<int>order(26,0);
-        int val=26;
-        for(int i=0;i<26;i++){
-            order[i]=val;
-            val--;
-        }
+        // vector<int>order(26,0);
+        // int val=26;
+        // for(int i=0;i<26;i++){
+        //     order[i]=val;
+        //     val--;
+        // }
+        
         for(int i=0;i<s.length();i++){
-            ans+=order[s[i]-'a']*(i+1);
+            int rev=26-(s[i]-'a');
+            ans+=rev*(i+1);
         }
         return ans;
     }
