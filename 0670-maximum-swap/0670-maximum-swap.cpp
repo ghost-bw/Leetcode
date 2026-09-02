9cb12b85b6c09c336749bc3322fb94ade5004c35
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-09-02 17:46:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (64.3%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        string str=to_string(num);
-        int l=0;
-        int ans=num;
-        for(int i=0;i<str.length();i++){
-            for(int j=i+1;j<str.length();j++){
-                if(str[i]<str[j]){
-                    swap(str[i],str[j]);
-                    ans=max(ans,stoi(str));
-                    swap(str[i],str[j]);
+        // string str=to_string(num);
+        // int l=0;
+        // int ans=num;
+        // for(int i=0;i<str.length();i++){
+        //     for(int j=i+1;j<str.length();j++){
+        //         if(str[i]<str[j]){
+        //             swap(str[i],str[j]);
+        //             ans=max(ans,stoi(str));
+        //             swap(str[i],str[j]);
+        //         }
+        //     }
+        // }
+        // return ans;
+        string str = to_string(num);
+        int n = str.length();
+        
+        // Record the last occurrence index of each digit (0-9)
+        vector<int> last_pos(10, -1);
+        for (int i = 0; i < n; i++) {
+            last_pos[str[i] - '0'] = i;
+        }
+        
+        // Scan from left to right to find the first digit we can improve
+        for (int i = 0; i < n; i++) {
+            // Check if there is a larger digit (9 down to current digit + 1)
+            // that appears later in the string
+            for (int d = 9; d > str[i] - '0'; d--) {
+                if (last_pos[d] > i) {
+                    // Swap them, and we are done! (Greedy choice)
+                    swap(str[i], str[last_pos[d]]);
+                    return stoi(str); 
                 }
             }
         }
-        return ans;
+
+        return num;
+
     }
 };
