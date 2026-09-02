9e4f2e9369b8af2cbeb690094acbf621aef02048
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-09-02 21:47:45 +0530

    Time: 10 ms (6.18%), Space: 23.2 MB (74.9%) - LeetHub

--- 3028-ant-on-the-boundary/3028-ant-on-the-boundary.cpp
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.cpp
@@ -4,12 +4,8 @@ public:
        int sum=nums[0];
        int count=0;
         for(int i=1;i<nums.size();i++){
-            if(sum>=0 && (sum+nums[i])==0){
-                count++;
-            }else if(sum<=0 && (sum+nums[i])==0){
-                count++;
-            }
-            sum+=nums[i];
+           sum+=nums[i];
+           if(sum==0)count++;
         }
         return count;
     }
