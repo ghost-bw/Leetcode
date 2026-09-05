fc047b56eec4872a020ee7bca738d556de527e5e
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-09-05 15:55:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.46%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
+    double power(double x, int n){
+        if(n==0) return 1.0;
+        double half=power(x,n/2);
+        if(n%2==0){
+            return half*half;
+        }else{
+            return half*half*x;
+        }
+    }
     double myPow(double x, int n) {
-
-        long long N = n; 
-        
-
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-        
-        double ans = 1.0;
-        
-
-        while (N > 0) {
-            if (N % 2 == 1) {
-                ans *= x;
-            }
-
-            x *= x;
-            N /= 2;
-        }
-        
-        return ans;
+        long long exp=n;
+        if(n<0) return power(1/x,-exp);
+        return power(x,exp);
     }
 };
