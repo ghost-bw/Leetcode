05f1557ab2f982029c91f3fd8c928f1ac83c9aa9
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-09-10 12:31:27 +0530

    Time: 27 ms (27.75%), Space: 9.7 MB (40.01%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
-    int dp[1001][1001];
+    int minSteps(int n) {
+        vector<int> dp(n + 1, 0);
 
-    int solve(int curr, int copied, int n,bool canCopy) {
-        if (curr == n)
-            return 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = i;
 
-        if (curr > n)
-            return 1e9;
-
-        if (dp[curr][copied] != -1)
-            return dp[curr][copied];
-
-        int copy = 1e9;
-        if (canCopy && curr != copied) { 
-            copy = 1 + solve(curr, curr, n, false); // canCopy is set to false for the next call
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    dp[i] = min(dp[i], dp[j] + i / j);
+                }
+            }
         }
 
-        int paste = 1e9;
-
-        if (copied > 0)
-            paste = 1 + solve(curr + copied, copied, n,true);
-
-        return dp[curr][copied] = min(copy, paste);
-    }
-
-    int minSteps(int n) {
-        if (n == 1)
-            return 0;
-
-        memset(dp, -1, sizeof(dp));
-
-        return solve(1, 0, n, true);
+        return dp[n];
     }
 };
