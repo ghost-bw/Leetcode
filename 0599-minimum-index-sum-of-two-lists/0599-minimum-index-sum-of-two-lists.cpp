8d7e34b623ab0193f8164a7fa0d6ea9a2c8c6dca
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-09-12 03:09:35 +0530

    Time: 17 ms (68.14%), Space: 41.2 MB (62.48%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    int sum(vector<int> &nums){
-        return nums[0]+nums[1];
-    }
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        unordered_map<string,vector<int>> mp;
+        unordered_map<string, int> mp;
+
+        for (int i = 0; i < list1.size(); i++) {
+            mp[list1[i]] = i;
+        }
+
+        int mn = INT_MAX;
         vector<string> ans;
-        for(int i=0;i<list1.size();i++){
-            mp[list1[i]].push_back(i);
-        }
-        for(int i=0;i<list2.size();i++){
-            mp[list2[i]].push_back(i);
-        }
-        int mn=INT_MAX;
-        for(auto &it: mp){
-            if(it.second.size()==2){
-                mn=min(mn,sum(it.second));
+
+        for (int i = 0; i < list2.size(); i++) {
+            if (mp.count(list2[i])) {
+                int sum = i + mp[list2[i]];
+
+                if (sum < mn) {
+                    mn = sum;
+                    ans.clear();
+                    ans.push_back(list2[i]);
+                }
+                else if (sum == mn) {
+                    ans.push_back(list2[i]);
+                }
             }
         }
-        for(auto &it: mp){
-            if(it.second.size()==2 && sum(it.second)==mn){
-                ans.push_back(it.first);
-            }
-        }
+
         return ans;
     }
 };
