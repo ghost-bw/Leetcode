34f0b6bd0285eae9cf3b1d20d698499ad162a1d0
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-09-11 21:19:51 +0530

    Time: 0 ms (100%), Space: 11.8 MB (73.91%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> freq(26,0);
+        for(char ch:magazine){
+            freq[ch-'a']++;
+        }
+        for(int i=0;i<ransomNote.size();i++){
+            if(freq[ransomNote[i]-'a']<=0)return false;
+            freq[ransomNote[i]-'a']--;
+        }
+        return true;
+    }
+};
