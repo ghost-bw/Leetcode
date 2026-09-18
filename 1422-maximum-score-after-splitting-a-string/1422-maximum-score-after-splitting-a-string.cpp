f85d0aa5ce0549176177f65a56f9c332b35fba9f
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-09-18 23:44:00 +0530

    Time: 0 ms (100%), Space: 8.9 MB (33.07%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int ones=0;
+        for(char ch:s){
+            if(ch=='1')ones++;
+        }
+        int ans=INT_MIN;
+        int sum=0;
+        int zeroes=0;
+        for(int i=0;i<s.length()-1;i++){
+            if(s[i]=='0'){
+                zeroes++;
+            }else {
+                ones--;
+            }
+            ans=max(ans,zeroes+ones);
+        }
+        return ans;
+    }
+};
