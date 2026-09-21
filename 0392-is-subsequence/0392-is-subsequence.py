bbd0193d83e8a69159f7e615212a4e4f0c717cfd
Raunak_Prabhakar <b23cs041@nitm.ac.in>
2026-09-21 21:56:55 +0530

    Time: 0 ms (100%), Space: 19.4 MB (7.93%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if(len(s)==0):
+            return True
+        i,j=0,0
+        for i in range(0,len(t)):
+            if(s[j]==t[i]):
+                j+=1;
+            else:
+                continue
+            if(j==len(s)):
+                return True
+        return True if(j==len(s)) else False
